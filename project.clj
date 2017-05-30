d7b290db3fd3df0823acb13c9178329419e66ceb
kishiguro <ishi@hash-set.com>
2017-05-30 11:53:55 -0700

    Add grpc and network.ip.

--- project.clj
+++ project.clj
@@ -3,7 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [kovacnica/clojure.network.ip "0.1.1"]
+                 [io.grpc/grpc-core "1.3.0"]
+                 [io.grpc/grpc-netty "1.3.0" :exclusions [io.grpc/grpc-core]]
+                 [io.grpc/grpc-protobuf "1.3.0"]
+                 [io.grpc/grpc-stub "1.3.0"]
+                 [org.clojure/data.json "0.2.6"]]
   :main ^:skip-aot zebra-clj.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
